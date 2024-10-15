a417a31dd5584e1cfd074c81e201e80e6bff9d02
Irene Pham <irenepham@Irenes-MacBook-Pro.local>
2024-10-15 07:19:43 -0700

    cleaning up code

--- ResponseLengthSelector.js
+++ ResponseLengthSelector.js
@@ -11,7 +11,7 @@ const ResponseLengthSelector = () => {
   const [lengthOptions, setLengthOptions] = useState([100, 300]);
   const [isAnyLength, setIsAnyLength] = useState(true); //by default it can be any length
 
-  //runs once at the beginning and then at the end during cleanup
+  //detect clicks outside of the dropdown
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -25,47 +25,32 @@ const ResponseLengthSelector = () => {
     };
   }, []);
 
-  //when clicking enter it adds the prefix and we see trusted message once
   useEffect(() => {
-    console.log("in use effect for button listening");
     const handleSubmit = (event) => {
       event.stopImmediatePropagation();
-      console.log("in handle submit click detected");
 
-      if (event.isTrusted != null && event.isTrusted == false) {
-        console.log("event is not trusted");
-        return;
-      }
-      console.log("event is trusted");
-
-      queueMicrotask(() => {
-        const form = event.target.closest("form");
-        if (form) {
-          const p = form.querySelector("p");
-          if (p) {
-            p.textContent = "tell me a joke " + p.textContent;
-            console.log("Submitting form with value:", p.textContent);
-          }
+      const form = event.target.closest("form");
+      if (form) {
+        const p = form.querySelector("p");
+        console.log("form submitted", isAnyLength);
+        if (p && !isAnyLength) {
+          p.textContent =
+            `in less than ${selectedLength} words ` + p.textContent;
+          console.log("Submitting form with value:", p.textContent);
         }
-      });
-      const newEvent = new MouseEvent("click", {
-        bubbles: true,
-        cancelable: true,
-        view: window,
-        isTrusted: false,
-      });
-      event.target.dispatchEvent(newEvent);
+      }
+
+      setTimeout(attachSendListeners, 3000);
     };
 
-    // when send button is enabled add text doesnt work cause then the user sees it
-    //TODO need to check for when the send button detaches again, also handle enter key
-    const attachListeners = () => {
-      //select butttons that are not disabled
-      const sendButton = document.querySelector(
-        'button[data-testid="send-button"]:not(:disabled)'
+    const attachSendListeners = () => {
+      //select butttons that are not disabled and not the stop button
+      let sendButton = document.querySelector(
+        'button[data-testid="send-button"]:not(:disabled):not([data-testid="stop-button"])'
       );
+      console.log("looking for send button");
       if (sendButton) {
-        console.log("found the send button");
+        console.log("found the send button", sendButton);
         sendButton.addEventListener(
           "click",
           (event) => handleSubmit(event),
@@ -85,12 +70,11 @@ const ResponseLengthSelector = () => {
         }
       } else {
         console.log("did not find the send button");
-        // If the button isn't found, try again after a short delay
-        setTimeout(attachListeners, 3000);
+        setTimeout(attachSendListeners, 3000);
       }
     };
 
-    attachListeners();
+    attachSendListeners();
 
     return () => {
       const sendButton = document.querySelector(
@@ -101,42 +85,36 @@ const ResponseLengthSelector = () => {
         sendButton.removeEventListener("mousedown", handleSubmit);
       }
     };
-  }, []);
+  }, [isAnyLength, selectedLength]);
 
-  //its adding another p, if theres a p with class placeholder, if theres already text then append it to the front
-  useEffect(() => {
-    const updatePromptTextarea = () => {
-      console.log("in updatePromptTextarea");
-      const promptTextarea = document.getElementById("prompt-textarea");
-      const placeholder = promptTextarea.querySelector("p.placeholder");
-      if (promptTextarea && selectedLength && !isAnyLength) {
-        if (placeholder) {
-          const instruction = document.createElement("p");
-          instruction.className = "length-instruction";
-          instruction.textContent = `in under ${selectedLength} words or less`;
-          promptTextarea.appendChild(instruction);
-          // Remove the first child of the promptTextarea
-          if (
-            promptTextarea.firstChild &&
-            promptTextarea.firstChild.textContent.trim() === ""
-          ) {
-            console.log("removing first child");
-            promptTextarea.removeChild(promptTextarea.firstChild);
-          }
-        } else {
-          console.log(
-            "appending instruction to first child since there is already text"
-          );
-          promptTextarea.firstChild.textContent =
-            `in under ${selectedLength} words or less` +
-            promptTextarea.firstChild.textContent;
-        }
-      }
-    };
-    updatePromptTextarea();
-  }, [selectedLength, isAnyLength]);
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  //   //its adding another p, if theres a p with class placeholder, if theres already text then append it to the front
+  //   useEffect(() => {
+  //     const updatePromptTextarea = () => {
+  //       console.log("in updatePromptTextarea");
+  //       const promptTextarea = document.getElementById("prompt-textarea");
+  //       const placeholder = promptTextarea.querySelector("p.placeholder");
+  //       if (promptTextarea && selectedLength && !isAnyLength) {
+  //         if (placeholder) {
+  //           // Remove the first child of the promptTextarea
+  //           if (
+  //             promptTextarea.firstChild &&
+  //             promptTextarea.firstChild.textContent.trim() === ""
+  //           ) {
+  //             console.log("removing first child");
+  //             promptTextarea.removeChild(promptTextarea.firstChild);
+  //           }
+  //         } else {
+  //           console.log(
+  //             "appending instruction to first child since there is already text"
+  //           );
+  //           promptTextarea.firstChild.textContent =
+  //             `in under ${selectedLength} words or less` +
+  //             promptTextarea.firstChild.textContent;
+  //         }
+  //       }
+  //     };
+  //     updatePromptTextarea();
+  //   }, [selectedLength, isAnyLength]);
 
   const handleSelectLength = (length) => {
     console.log("handle option change", length);
@@ -147,7 +125,7 @@ const ResponseLengthSelector = () => {
 
   return (
     <div ref={dropdownRef} className="length-button-container">
-      <button onClick={toggleDropdown} className="length-button">
+      <button onClick={() => setIsOpen(!isOpen)} className="length-button">
         <span>
           {selectedLength ? ` < ${selectedLength} words` : "Response Length"}
         </span>
@@ -169,7 +147,15 @@ const ResponseLengthSelector = () => {
               length={length}
             />
           ))}
-          <button onClick={() => setIsAnyLength(true)}>Any Length</button>
+          <button
+            onClick={() => {
+              setIsAnyLength(true);
+              setIsOpen(false);
+              setSelectedLength(null);
+            }}
+          >
+            Any Length
+          </button>
         </div>
       )}
     </div>
--- ScrollUpButton.js
+++ ScrollUpButton.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const ScrollUpButton = () => {
   const [isAtBottom, setIsAtBottom] = React.useState(false);
+  const [isArticleLong, setIsArticleLong] = React.useState(false);
 
   const scrollToLastArticle = () => {
     const articles = document.getElementsByTagName("article");
@@ -13,18 +14,42 @@ const ScrollUpButton = () => {
 
   //checks if the end of the last article is at the bottom of the page
   const checkIfAtBottom = () => {
-    const articles = document.getElementsByTagName("article");
+    const articles = document.querySelectorAll("article div[class*='m-auto']");
     if (articles.length > 0) {
       const lastArticle = articles[articles.length - 1];
       const rect = lastArticle.getBoundingClientRect();
+      //console.log(rect.bottom, window.innerHeight);
       const isAtBottom = rect.bottom <= window.innerHeight;
       setIsAtBottom(isAtBottom);
     }
   };
 
+  //checks if the last article is long
+  const checkIfArticleIsLong = () => {
+    const articles = document.querySelectorAll("article div[class*='m-auto']");
+    const hfullDiv = document.querySelector("main div.h-full");
+    if (articles.length > 0 && hfullDiv) {
+      const lastArticle = articles[articles.length - 1];
+      const lastArticleHeight = lastArticle.offsetHeight;
+      const hfullHeight = hfullDiv.offsetHeight;
+      //console.log("Height of h-full div:", hfullHeight);
+      const adjustedHeight = hfullHeight - 100;
+      //console.log("Adjusted height:", adjustedHeight);
+      //console.log("last article height", lastArticleHeight);
+      //console.log(lastArticleHeight > adjustedHeight);
+      setIsArticleLong(lastArticleHeight > adjustedHeight);
+    } else {
+      console.log("no articles or hfull div found", articles, hfullDiv);
+    }
+    return null;
+  };
+
   React.useEffect(() => {
     const intervalId = setInterval(() => {
       checkIfAtBottom();
+      checkIfArticleIsLong();
+      //   console.log("isAtBottom", isAtBottom);
+      //   console.log("isArticleLong", isArticleLong);
     }, 2000);
 
     return () => {
@@ -33,7 +58,8 @@ const ScrollUpButton = () => {
   }, []);
 
   return (
-    isAtBottom && (
+    isAtBottom &&
+    isArticleLong && (
       <button
         style={{
           position: "fixed",
